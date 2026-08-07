5196cd8b6d7678ee6301b232d95f2118e4d84c5c
francescoavitto88 <thewall1988@hotmail.it>
2026-08-07 17:59:01 +0200

    refactoring, Habit Form component

---
+++ src/components/HabitForm.tsx
@@ -0,0 +1,8 @@
+import { Button } from "./Button";
+
+export function HabitForm() {
+  return <form className="flex gap-2">
+    <input type="text" placeholder="New habit..." className="flex-1 rounded-lg bg-zinc-800 px-4 py-2 outline-none focus-visible:ring-2 focus-visible:ring-violet-500"/>
+    <Button >Add Habit</Button>
+  </form>;
+}
