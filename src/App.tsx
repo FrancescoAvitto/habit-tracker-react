24525a21fc0f703d16b816e78ff0a2e3f25eb82c
francescoavitto88 <thewall1988@hotmail.it>
2026-08-10 17:54:03 +0200

    toggle habit functions

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,37 @@ import { useState } from "react";
 import { HabitForm } from "./components/HabitForm";
 import { HabitList, type Habit } from "./components/HabitList";
 import { Header } from "./components/Header";
+import { isSameDay } from "date-fns/fp/isSameDay";
 
 export default function App() {
   const [habits, setHabits] = useState<Habit[]>([]);
 
   function addHabit(name: string){
-    setHabits([...habits, {id: crypto.randomUUID(), name}]);
+    setHabits([...habits, {id: crypto.randomUUID(), name, completions:[]}]);
   }
 
     function deleteHabit(id: string){
     setHabits(curr => curr.filter((habit) => habit.id !== id));
   }
 
+  function toggleHabit(id:string, date:Date){
+    setHabits(curr =>
+      curr.map(h => {
+        if(h.id !== id) return h;
+
+        const alreadyDone = h.completions.some(c=> isSameDay(c,date))
+
+        const completions = alreadyDone ? h.completions.filter(c => !isSameDay(c,date)) : [...h.completions, date];
+        return {...h, completions};
+  }),
+)
+}
+
 
   return <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
   <Header />
   <HabitForm addHabit={addHabit} />
-  <HabitList habits={habits} deleteHabit={deleteHabit} />
+  <HabitList habits={habits} deleteHabit={deleteHabit} toggleHabit={toggleHabit} />
   </div>;
 }
 
--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -1,17 +1,19 @@
 import { Button } from "./Button";
-import {eachDayOfInterval, endOfWeek, format, isFuture, startOfWeek} from "date-fns"
+import {eachDayOfInterval, endOfWeek, format, isFuture, isSameDay, startOfWeek} from "date-fns"
 
 export type Habit = {
   id: string,
   name: string
+  completions: Date[]
 }
 
 type HabitListProps = {
   habits: Habit[]
   deleteHabit: (id: string) => void
+  toggleHabit: (id: string, date: Date) => void
 }
 
-export function HabitList({habits, deleteHabit}: HabitListProps) {
+export function HabitList({habits, deleteHabit, toggleHabit}: HabitListProps) {
 
   // const habits = [{ id: 'dijg', name: "Drink Water" }, { id: 'dhai1', name: "Exercise" }];
   
@@ -21,7 +23,7 @@ export function HabitList({habits, deleteHabit}: HabitListProps) {
 
   return <div className="flex flex-col gap-3">
     {habits.map((habit) => (
-      <HabitItem deleteHabit={deleteHabit} key={habit.id} habit={habit} />
+      <HabitItem deleteHabit={deleteHabit} toggleHabit={toggleHabit} key={habit.id} habit={habit} />
     ))}
   </div>
 }
@@ -29,9 +31,10 @@ export function HabitList({habits, deleteHabit}: HabitListProps) {
 type HabitItemProps = {
   habit: Habit
   deleteHabit: (id: string) => void
+  toggleHabit: (id: string, date: Date) => void
 }
 
-function HabitItem({habit, deleteHabit}: HabitItemProps){
+function HabitItem({habit, deleteHabit, toggleHabit}: HabitItemProps){
   const visibleDates= eachDayOfInterval({
     start: startOfWeek(new Date(), {weekStartsOn: 1}),
     end: endOfWeek(new Date(), {weekStartsOn: 1})
@@ -50,7 +53,13 @@ function HabitItem({habit, deleteHabit}: HabitItemProps){
     </div>
 
     <div className="flex gap-1.5" >
-      {visibleDates.map((date) => (<Button className="flex flex-1 flex-col items-center gap-0.5 rounded-lg text-xs" key={date.toISOString()} disabled={isFuture(date)}>
+      {visibleDates.map((date) => (
+        <Button 
+            className="flex flex-1 flex-col items-center gap-0.5 rounded-lg text-xs" 
+            key={date.toISOString()} 
+            disabled={isFuture(date)} 
+            onClick={() => toggleHabit(habit.id, date)}
+            variant={habit.completions.some(d=> isSameDay(date,d)) ? "primary" : "secondary"}>
         <span className="font-medium">{format(date, "EEE")}</span>
         <span>{format(date, "d")}</span>
       </Button>))}
