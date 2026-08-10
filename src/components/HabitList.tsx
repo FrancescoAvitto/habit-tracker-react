223ebece13b6918f2fef5484adbb1fe122620d63
francescoavitto88 <thewall1988@hotmail.it>
2026-08-10 19:41:59 +0200

    render streak

--- src/components/HabitList.tsx
+++ src/components/HabitList.tsx
@@ -47,7 +47,8 @@ function HabitItem({habit, deleteHabit, toggleHabit}: HabitItemProps){
       <div className="flex flex-col gap-1">
         <div className="flex items-center gap-3">
           <span className="font-medium">{habit.name}</span>
-          <span className="text-sm text-amber-400">🔥{streak}</span>
+          {streak > 0 && (<span className="text-sm text-amber-400">🔥 {streak}</span>)}
+          
         </div>
 
      </div>
