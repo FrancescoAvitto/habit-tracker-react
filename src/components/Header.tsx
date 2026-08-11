200d4dedb491f8b0fea3ab5b7966df561bd18d40
francescoavitto88 <thewall1988@hotmail.it>
2026-08-11 13:23:55 +0200

    prev and next

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,20 @@
 import { useHabits } from "../context/HabitProvider";
 import { Button } from "./Button";
 import { isToday } from "date-fns/isToday";
+import { format } from "date-fns";
 
-export function Header() {
+type HeaderProps = {
+  visibleDates: Date[];
+  onPrev: () => void;
+  onNext: () => void;
+}
+
+export function Header({ visibleDates, onPrev, onNext }: HeaderProps) {
   const { habits } = useHabits();
 
   const doneToday = habits.filter(h => h.completions.some(c => isToday(c))).length;
+
+  const dateRange = `${format(visibleDates[0], "MMM d")} - ${format(visibleDates[visibleDates.length - 1], "MMM d")}`;
 
 
   return <header className="flex items-center justify-between">
@@ -14,10 +23,10 @@ export function Header() {
       <span className="text-sm text-zinc-400">{doneToday}/{habits.length} done today</span>
     </div>
     <div className="flex flex-col gap-1 items-end">
-       <span className="text-sm text-zinc-400">Apr 6 - Apr 12</span>
+       <span className="text-sm text-zinc-400">{dateRange}</span>
        <div className="flex items-center gap-3">
-        <Button >Prev</Button>
-        <Button >Next</Button>
+        <Button onClick={onPrev}>Prev</Button>
+        <Button onClick={onNext}>Next</Button>
        </div>
     </div>
   </header>;
