42c3065e5053d85e0f65c9aca0fa47d9da455899
francescoavitto88 <thewall1988@hotmail.it>
2026-08-11 12:49:15 +0200

    done today in habits lenght

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,17 @@
+import { useHabits } from "../context/HabitProvider";
 import { Button } from "./Button";
+import { isToday } from "date-fns/isToday";
 
 export function Header() {
+  const { habits } = useHabits();
+
+  const doneToday = habits.filter(h => h.completions.some(c => isToday(c))).length;
+
+
   return <header className="flex items-center justify-between">
     <div className="flex flex-col gap-1">
       <h1 className="text-3xl font-bold">Habit Tracker</h1>
-      <span className="text-sm text-zinc-400">1/1 done today</span>
+      <span className="text-sm text-zinc-400">{doneToday}/{habits.length} done today</span>
     </div>
     <div className="flex flex-col gap-1 items-end">
        <span className="text-sm text-zinc-400">Apr 6 - Apr 12</span>
